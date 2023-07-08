678dd9da5aefa81ae13da566e23f836410fb41cd
shenzuni <gianpmaule@gmail.com>
2023-07-08 18:21:28 -0300

    add script

---
+++ script.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Script for passeidireto.com paywall removal
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Remove paywall
+// @author       shenzuni
+// @match        *://www.passeidireto.com/*
+// @grant        none
+// ==/UserScript==
+(function() {
+    'use strict';
+    window.addEventListener('load', () => setInterval(() => {
+        let textBlurs = document.querySelectorAll('[style="filter: blur(10px);"]');
+        textBlurs.forEach(textBlur => {
+            textBlur.removeAttribute("style");
+        });
+        let unlocks = document.querySelectorAll('.limitation-unlock-rewards, .mv-content-limitation-fake-page');
+        unlocks.forEach(unlock => {
+            unlock.remove();
+        });
+    }, 2000));
+})();
